22a5317bc0729bbb4639cb8e567dab19d76ca631
HO-Vic <shp9826@naver.com>
2025-11-29 19:37:34 +0900

    feat: NetTestScene Add ConnBtn

---
+++ Assets/Scenes/DevScene/NetworkTest/ConnBtn.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ConnBtn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDisconn(hunt.Net.NetModule.ERROR e, string msg)
+    {
+        Debug.Log(msg);
+    }
+
+    void OnConnSucc()
+    {
+        Debug.Log("Conn Succ");
+    }
+
+    void OnConnFail(SocketException e)
+    {
+        Debug.Log(e.Message);
+    }
+
+    public void OnConnBtn()
+    {
+        Debug.Log("On ConnBtn");
+        hunt.Net.NetworkManager.Shared.ConnLoginServerSync(OnDisconn, OnConnSucc, OnConnFail);
+    }
+}
--- Assets/Script/Network/NetworkManager.cs
+++ Assets/Script/Network/NetworkManager.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using UnityEngine;
 using static hunt.Net.NetModule;
 //using hunt.Net;
@@ -17,6 +18,23 @@ namespace hunt.Net
         public NetModule MakeNetModule(ServiceType type, Action<NetModule.ERROR, string>? disconnectHandler, Action connSuccessHandler, Action<SocketException> connFailHandler)
         {
             return new NetModule(type, disconnectHandler, connSuccessHandler, connFailHandler);
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public async Task<bool> ConnLoginServer(Action<NetModule.ERROR, string>? disconnectHandler, Action connSuccessHandler, Action<SocketException> connFailHandler)
+        {
+            m_loginConnection = new NetModule(ServiceType.Login, disconnectHandler, connSuccessHandler, connFailHandler);
+            return await m_loginConnection.AsyncConn("127.0.0.1", 9000);
+        }
+
+        public bool ConnLoginServerSync(Action<NetModule.ERROR, string>? disconnectHandler, Action connSuccessHandler, Action<SocketException> connFailHandler)
+        {
+            m_loginConnection = new NetModule(ServiceType.Login, disconnectHandler, connSuccessHandler, connFailHandler);
+            return m_loginConnection.SyncConn("127.0.0.1", 9000);
         }
 
         public bool IsExistConnection(UInt64 key)
